133ed51d7da5cfcc030d08db98053359ba264865
ElJako11 <joaquimejuhah@gmail.com>
2026-01-04 23:45:15 -0400

    Added file for Railway deploy

--- server/index.js
+++ server/index.js
@@ -1,34 +1,41 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "../clientechat")));
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../clientechat/index.html"));
+});
+
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log('Usuario conectado:', socket.id);
+io.on("connection", (socket) => {
+  console.log("Usuario conectado:", socket.id);
 
-    socket.on('mensaje', (data) => {
-        io.emit('mensaje', {
-            nombre: data.nombre, 
-            texto: data.texto,
-            id: socket.id.slice(0, 5)
-        });
+  socket.on("mensaje", (data) => {
+    io.emit("mensaje", {
+      nombre: data.nombre,
+      texto: data.texto,
+      id: socket.id.slice(0, 5),
     });
+  });
 
-    socket.on('disconnect', () => {
-        console.log('Usuario desconectado:', socket.id);
-    });
+  socket.on("disconnect", () => {
+    console.log("Usuario desconectado:", socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 6969;
